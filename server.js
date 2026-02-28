c254760d9623c6972fef4ec543792933b95e5d88
Rajveer(Chiranjeev)Shekhawat <121601167+rajvveer@users.noreply.github.com>
2026-02-28 11:42:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'https://solaramb.netlify.app' }));
+app.use(cors({ origin: 'https://warm-croquembouche-6ee21f.netlify.app' }));
 
 app.use(express.json());
 
